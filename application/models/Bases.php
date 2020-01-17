c29a20f07bb7ed70715546fea3d40918542e8ad7
andrees0801 <58889000+andrees0801@users.noreply.github.com>
2020-01-16 23:30:47 -0600

    consultas random y obtencion de informacion de sucursales

--- application/models/Bases.php
+++ application/models/Bases.php
@@ -31,7 +31,33 @@ class bases extends CI_Model {
       return $query->result();
     }
 
-    /* Fin consultas categorias, subcategorias y secciones */
+		/* Fin consultas categorias, subcategorias y secciones */
+		
+		/* Funciones de vista principal */
+
+		public function obtener_categorias_rand()
+		{
+			$sql = "SELECT `id_categorias`, `categoria` FROM `categorias` WHERE 1 ORDER BY RAND()";
+			$query = $this->db->query($sql);
+
+			return $query->result();
+		}
+
+		public function get_sucursales_categorias($id_categoria)
+		{
+			$sql = "SELECT e.id_empresa, e.nombre, e.foto_perfil, suc.id_sucursal, z.zona FROM empresa as e inner join categoria as c inner join subcategoria as sub inner join secciones as s inner join sucursal as suc inner join direccion as dir inner join zona as z on c.id_secciones = s.id_secciones and s.id_subcategoria = sub.id_subcategoria and suc.id_empresa = e.id_empresa and suc.id_direccion = dir.id_direccion and dir.id_zona = z.id_zona WHERE sub.id_categoria LIKE '".$id_categoria."' ORDER BY RAND()";
+			$query = $this->db->query($sql);
+			
+			if($query->num_rows() > 0)
+			{
+				return $query->result();
+			}else{
+				return FALSE;
+			}
+		}
+
+
+		/* Fin consultas vista principal */
 
 }
 ?>
